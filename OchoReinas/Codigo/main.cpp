f73bee942dafed00b3d7c3cc70a814f210107506
RichardAlban <rsalban3@espe.edu.ec>
2022-02-18 07:50:12 -0500

    8Reinas

---
+++ OchoReinas/Codigo/main.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+#include <fstream>  
+#include "Reina.h"
+#include "Ingreso.h"
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	int n;
+	cout<<"\tCuantas reinas: "<<endl;
+	cin>>n;
+	Reina *obj= new Reina(n);
+	obj->solucionReinas();
+	cout<<"Se creo el archivo solucion.txt"<<endl;
+
+	return 0;
+}
+
